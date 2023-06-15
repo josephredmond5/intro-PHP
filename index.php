671ab4c7f5385b092f2b1b7faad0bc196585463a
josephredmond5 <josephredmond5@icloud.com>
2023-06-15 02:09:44 +0100

    intro to php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+define('NAME', 'yoshi');
+
+//$name = 'yoshi';
+$age = 30;
+
+//$name = 'mario';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> php tutorials </title>
+</head>
+<body>
+    <h1>user profile page</h1>
+
+    <div><?php echo $name; ?></div>
+    <div><?php echo $age; ?></div>
+
+</body>
+</html>
