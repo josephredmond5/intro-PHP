9e319a77f3172a93cba510201b2cb2cb8c8622ac
josephredmond5 <josephredmond5@icloud.com>
2023-06-16 00:53:29 +0100

    numbers in php

--- numbers.php
+++ numbers.php
@@ -1,5 +1,35 @@
 <?php 
 
+$radius = 25;
+$pi = 3.14;
+
+// basic operators - *, /, +, -, **
+
+echo $pi * $radius**2;
+
+// order of operation (B I D M A S) brackets, indicies, division, multiplication, addition, subtraction
+
+echo 2 * (4 + 9) / 3;
+
+// increment & decrement operators
+
+echo $radius++; // this is showing 25 because it is reading $radius first
+echo $radius; // this is now showing 26
+
+// shorthand operators
+
+$age = 20;
+$age += 10; // shorthand operator for adding something
+$age -= 10;  // shorthand operator for subtracting something
+$age *= 10; // shorthand operator for multiplying
+echo $age;
+
+// number functions
+
+echo floor($pi); // this takes the number down
+echo ceil($pi); // moves it up to the nearest integer
+echo pi();
+
 
 ?>
 
