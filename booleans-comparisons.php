1e207f75acb3836dfa5bd0c90030d6fc67e5cc1e
josephredmond5 <josephredmond5@icloud.com>
2023-06-16 16:14:01 +0100

    booleans and comparisons

--- booleans-comparisons.php
+++ booleans-comparisons.php
@@ -1,25 +1,37 @@
 <?php 
 
-$products = [
-    ['name' => 'shiny steel', 'price' => 20],
-    ['name' => 'shiny steel', 'price' => 20],
-    ['name' => 'shiny steel', 'price' => 20],
-    ['name' => 'shiny steel', 'price' => 20],
-    ['name' => 'shiny steel', 'price' => 20],
-    ['name' => 'shiny steel', 'price' => 20]
-];
-
-while($i < count($products)){
-    echo $products[$i]['name'];
-    echo '<br />';
-    $i++;
-}
-
 
 // comparison booleans
+echo true; // values have to be converted to strings and string of 1 if true - "1"
+echo false; // empty string if false, ""
+
+
+// numbers
+echo 5 < 10; //  this is true so equates to 1 in the browser 
+echo 5 > 10;
+echo 5 == 10;
+echo 10 == 10;
+echo 5 != 10;
+echo 5 <= 5;
+echo 5 >= 5;
+
+// strings
+echo 'shaun' < 'yoshi'; // 'shaun' comes first because 's' comes first in the alphabet
+echo 'shaun' > 'yoshi';
+echo 'shaun' > 'Shaun'; // an uppercase letter is seen as less than a normal letter
+echo 'mario' == 'mario';
+echo 'mario' == 'Mario';
 
 
 // loose vs strict booleans
+echo 5 == '5'; // this is a loose comparison and doesnt take into account the type of data
+echo 5 === '5'; // this is a strict comparison and takes into account the type of data
+echo 5 === 5; // loose vs strict equal comparison
+
+
+echo true == "1"; // loose comparison
+echo false == ""; 
+
 
 
 
