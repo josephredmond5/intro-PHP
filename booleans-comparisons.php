8da290ca6f8c8189c0d604148dd01b2ed25960eb
josephredmond5 <josephredmond5@icloud.com>
2023-06-16 15:58:01 +0100

    booleans and comparisons in php

---
+++ booleans-comparisons.php
@@ -0,0 +1,37 @@
+<?php 
+
+$products = [
+    ['name' => 'shiny steel', 'price' => 20],
+    ['name' => 'shiny steel', 'price' => 20],
+    ['name' => 'shiny steel', 'price' => 20],
+    ['name' => 'shiny steel', 'price' => 20],
+    ['name' => 'shiny steel', 'price' => 20],
+    ['name' => 'shiny steel', 'price' => 20]
+];
+
+while($i < count($products)){
+    echo $products[$i]['name'];
+    echo '<br />';
+    $i++;
+}
+
+
+// comparison booleans
+
+
+// loose vs strict booleans
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> php booleans-comparisons </title>
+</head>
+<body>
+
+</body>
+</html>
