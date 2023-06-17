c7691a6458bba8a593cad5f9ca54e46bd5fa398e
josephredmond5 <josephredmond5@icloud.com>
2023-06-17 03:26:25 +0100

    functions in php

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php 
+
+// functions
+
+function sayHello($name = 'joseph', $time = 'morning'){
+    echo "good morning $name";
+}
+// sayHello('mario');
+sayHello('yoshi', 'night');
+
+// function formatProduct($product){
+//     // echo "{$product['name']} costs £{$product['price']} to buy <br />";
+//     return  "{$product['name']} costs £{$product['price']} to buy <br />"; 
+// }
+
+// $formatted = formatProduct(['name' => 'gold star', 'price' => 20]);
+// echo $formatted;
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> php functions </title>
+</head>
+<body>
+
+</body>
+</html>
