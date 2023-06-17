b5a51d43ab9839ffd4f88babc691b210e0801a96
josephredmond5 <josephredmond5@icloud.com>
2023-06-17 03:40:17 +0100

    variable scope in php

--- variable-scope.php
+++ variable-scope.php
@@ -5,6 +5,39 @@
 
 // local vars
 
+// function myFunc(){
+//     $price = 10;
+//     echo $price;
+// }
+
+// myFunc();
+
+// function myFuncTwo($age){ // when we create a variable inside a function it has local scope inside the function
+//     echo $age;
+// }
+// myFuncTwo(25);
+
+$name = 'mario';
+
+// function sayHello(){
+//     global $name; // this makes it a global variable
+//     $name = 'yoshi';
+//     echo "hello $name";
+// }
+
+// sayHello();
+// echo $name;
+
+
+function sayBye(&$name){ // when we use a parametr inside a function that it a local variable 
+    $name = 'wario';
+    echo "bye $name"; // dont forget semi colons (;)
+}
+
+sayBye($name);
+echo $name;
+
+
 
 ?>
 
