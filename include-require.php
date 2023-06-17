0725c453eb0dbb3a9288a609c06fe58659141caa
josephredmond5 <josephredmond5@icloud.com>
2023-06-17 03:49:14 +0100

    require and include in php

---
+++ include-require.php
@@ -0,0 +1,26 @@
+<?php 
+
+// include 
+include('ninjas.php'); // works with paranthesis
+include 'ninjas.php'; // also works without paranthesis
+echo 'end of php';
+
+// require
+// require('ninjas.php');
+
+// require('ninjass.php'); // require will not carry on with the code because its wrong
+// include('ninjass.php'); // include will carry on with the code if its wrong
+// echo 'end of php';
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> php include & require </title>
+</head>
+<body>
+
+</body>
+</html>
